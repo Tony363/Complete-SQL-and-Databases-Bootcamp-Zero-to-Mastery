b6de95415c736e920a6bb9b9f27a847bf99b4126
pysolver33 <pysolver33@gmail.com>
2021-09-06 22:00:01 +0800

    SQL is actually hard

--- exercises1.sql
+++ exercises1.sql
@@ -40,6 +40,7 @@
 -- FROM  "public"."customers"
 -- WHERE income > 50000 AND (age < 30 OR age > 50) 
 -- and (country = 'Japan' OR country = 'Australia')
+
 /*
 * DB: Store
 * Table: Orders
@@ -61,4 +62,101 @@
 * Question: 
 * Replace all empty first or last names with a default?
 */
--- SELECT COALESCE(lastName,'nameless') AS lastname ,COALESCE(name,'nameless') as firstname from "Student";
+-- SELECT COALESCE(lastName,'nameless') AS lastname ,COALESCE(name,'nameless') AS firstname from "Student";
+
+/*
+* DB: Store
+* Table: customers
+* Question: adjust the following query to display the null values as "No Address"
+*/
+-- SELECT COALESCE(address2,'No Address')  AS address
+-- SELECT address2
+-- FROM"public"."customers" 
+
+
+/*
+* DB: Store
+* Table: customers
+* Question: Fix the following query to apply proper 3VL
+*/
+
+-- SELECT *
+-- FROM public.customers
+-- WHERE address2 IS NOT null;
+
+/*
+* DB: Store
+* Table: customers
+* Question: Fix the following query to apply proper 3VL
+*/
+
+-- SELECT coalesce(lastName, 'Empty') AS lastName 
+-- from public.customers
+-- where (age IS NOT null);
+
+
+-- Who between the ages of 30 and 50 has an income less than 50 000?
+-- SELECT concat(firstname,' ',lastname)
+-- from public.customers
+-- WHERE income < 50000  AND (age between 30 AND 50)
+
+-- What is the average income between the ages of 20 and 50? (Including 20 and 50)
+-- select avg(income)
+-- from public.customers
+-- where age BETWEEN 20 and 50
+
+/*
+* DB: Store
+* Table: orders
+* Question: How many orders were made by customer 7888, 1082, 12808, 9623
+*/
+-- 
+-- SELECT count(1)
+-- FROM public.orders
+-- WHERE customerid in (7888,1082,12808,9623)
+
+/*
+* DB: World
+* Table: city
+* Question: How many cities are in the district of Zuid-Holland, Noord-Brabant and Utrecht?
+*/
+-- Select Count(1)
+-- from public.city
+-- WHERE district in ('Zuid-Holland','Noord-Brabant','Utrecht')
+
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Find the age of all employees who's name starts with M.
+* Sample output: https://imgur.com/vXs4093
+* Use EXTRACT (YEAR FROM AGE(birth_date)) we will learn about this in later parts of the course
+*/
+-- SELECT  first_name,EXTRACT (YEAR FROM AGE(birth_date)) as "age" 
+-- FROM public.employees
+-- WHERE first_name LIKE 'M%'
+
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many people's name start with A and end with R?
+* Expected output: 1846
+*/
+-- SELECT count(1)
+-- FROM public.employees
+-- WHERE first_name ilike 'A%r'
+
+/*
+* DB: Store
+* Table: customers
+* Question: How many people's zipcode have a 2 in it?.
+* Expected output: 4211 
+*/
+-- SELECT *
+-- FROM public.customers
+-- select count(1)
+-- from public.customers
+-- where zip::text like '%2%'
+
+-- show timezone
